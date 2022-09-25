34d902726b304725abe4b630ac932afb30f71b7b
Mark Perez <marcosek11@gmail.com>
2022-09-25 13:56:37 -0700

    Exception Added, and Mock Test Pending Progress

--- controller/customer_controller.py
+++ controller/customer_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, request
 
+from exception.customer_exists import CustomerAlreadyExistsError
+from exception.customer_not_found import CustomerNotFoundError
 from model.customer import Customer
 from service.customer_service import CustomerService
 
--- test/service/test_customer_service.py
+++ test/service/test_customer_service.py
@@ -1,4 +1,6 @@
 import dao.customer_dao
+from exception.customer_not_found import CustomerNotFoundError
+from exception.customer_exists import CustomerAlreadyExistsError
 from model.customer import Customer
 from service.customer_service import CustomerService
 import pytest
@@ -6,31 +8,28 @@ import pytest
 def test_get_all_customers(mocker):
     # ARRANGE
     def mock_get_all_customers(self):
-        return [Customer(1, 'Nicholas Albro', 'Nicholas', 'Albro', '572-945-7688', 'ogtempuraa'),
-                [Customer(2, 'Nicholas Jung', 'Nicholas', 'Jung', '818-352-0900', 'nickjung9')]
+        return [Customer(1, 'Nicholas Albro', 'Nicholas', 'Albro', '572-945-7688', 'ogtempuraa'), Customer(2, 'Nicholas Jung', 'Nicholas', 'Jung', '818-352-0900', 'nickjung9')]
     # ARRANGE
-    mocker.patch('dao.customer_dao.CustomerDao.get_all_customers', mock_get_all_customers)]
-
+    mocker.patch('dao.customer_dao.CustomerDao.get_all_customers', mock_get_all_customers)
     customer_service = CustomerService()
-
     # ACT
     actual = customer_service.get_all_customers()
     # ASSERT
     assert actual == [
         {
             "id": 1,
-            "customername": 'Nicholas Albro',
-            "first_name": 'Nicholas',
-            "last_name": 'Albro',
-            "customerphone": '572-945-7688',
-            "username": 'ogtempuraa'
+            "customername": "Nicholas Albro",
+            "first_name": "Nicholas",
+            "last_name": "Albro",
+            "customerphone": "572-945-7688",
+            "username": "ogtempuraa"
         },
         {
             "id": 2,
-            "customername": 'Nicholas Jung',
-            "first_name": 'Nicholas',
-            "last_name": 'Jung',
-            "customerphone": '818-352-0900',
-            "username": 'nickjung9'
+            "customername": "Nicholas Jung",
+            "first_name": "Nicholas",
+            "last_name": "Jung",
+            "customerphone": "818-352-0900",
+            "username": "nickjung9"
         }
     ]
